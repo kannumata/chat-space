0389919157cd7b46c7025f23dcc34fa406f7eed2
沼幹太 <kanta.mischil@icloud.com>
2018-08-12 14:36:54 +0900

    [edit]nameカラムの設定

--- db/migrate/20180812044405_create_groups.rb
+++ db/migrate/20180812044405_create_groups.rb
@@ -1,6 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.2]
   def change
     create_table :groups do |t|
+      t.string :name, null: false, unique: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_12_012048) do
+ActiveRecord::Schema.define(version: 2018_08_12_051435) do
+
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
