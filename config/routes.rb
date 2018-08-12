5ede4495240b44587db49969687026f2d00ecf03
沼幹太 <kanta.mischil@icloud.com>
2018-08-13 01:54:47 +0900

    groupのアクション設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
 end
