9bdf41f88b0b3b9cccf774587c64662a19de33a3
沼幹太 <kanta.mischil@icloud.com>
2018-08-12 14:37:20 +0900

    [edit]データベースのアソシエーション

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,7 @@
 class Member < ApplicationRecord
+  t.references :user,  null: false, foreign_key: true
+  t.references :group, null: false, foreign_key: true
+
+  belongs_to :group
+  belongs_to :user
 end
