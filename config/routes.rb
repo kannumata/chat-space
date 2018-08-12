530cc94a91d5ef4d851d52c0ee5be0e7c09485a4
沼幹太 <kanta.mischil@icloud.com>
2018-08-13 01:55:06 +0900

    messageのアクション設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root "messages#index"
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
