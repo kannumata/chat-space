3a719d6cef7c391af7aa586d203102a68b0debdd
沼幹太 <kanta.mischil@icloud.com>
2018-08-14 20:34:58 +0900

    [edit]ミスの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root 'messages#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+  end
 end
