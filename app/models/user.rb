a7866bed8543bc95af390510ae48a3144b7e90da
沼幹太 <kanta.mischil@icloud.com>
2018-08-16 13:04:37 +0900

    [edit]順番の入れ替え
    has_many :membersが下にあることでエラーが起こっていました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :messages
+  has_many :members
   has_many :groups ,through: :members
 end
