b968a02048a6f47152f814f0249a123e2dfc868e
沼幹太 <kanta.mischil@icloud.com>
2018-08-16 13:44:51 +0900

    [edit]set_groupアクションの設定
    edit, updateアクションの共通で起こしたいアクションをbefore_actionに定義しました

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -30,5 +31,9 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, {:user_ids => []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
