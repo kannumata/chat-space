e1f1dc1e490b4605d5f5d631a76aefc1d4db1d15
沼幹太 <kanta.mischil@icloud.com>
2018-08-20 16:15:55 +0900

    [edit]細かい変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,14 +23,16 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_message_path(@group), notice: "グループを編集しました"
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
+    end
   end
 
   private
+
   def group_params
-    params.require(:group).permit(:name, {:user_ids => []})
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 
   def set_group
