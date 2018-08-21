2a7af926cd2223f15dd4f66bfea1ba8a9918cc86
沼幹太 <kanta.mischil@icloud.com>
2018-08-21 13:57:11 +0900

    [edit]メッセージを降順に変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
+    @messages = @group.messages.includes(:user).order("id DESC")
   end
 
   def create
