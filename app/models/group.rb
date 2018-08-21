61138c2763adc8db5e1c003539ed6d6323f1920d
沼幹太 <kanta.mischil@icloud.com>
2018-08-21 11:51:24 +0900

    [edit]side_barにメッセージを表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :messages
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.body?
+       last_message.body
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージがありません'
+    end
+  end
 end
