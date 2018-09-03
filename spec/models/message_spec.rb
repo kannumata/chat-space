68cf8e7cd76a3080009a3f26d414899a3fe13e35
沼幹太 <kanta.mischil@icloud.com>
2018-09-03 23:46:56 +0900

    細かい変更

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe '#create' do
     context 'can save' do
-      it 'is valid with content' do
+      it 'is valid with body' do
         expect(build(:message, image: nil)).to be_valid
       end
 
@@ -11,16 +11,16 @@ RSpec.describe Message, type: :model do
         expect(build(:message, body: nil)).to be_valid
       end
 
-      it 'is valid with content and image' do
+      it 'is valid with body and image' do
         expect(build(:message)).to be_valid
       end
     end
 
     context 'can not save' do
-      it 'is invalid without content and image' do
+      it 'is invalid without body and image' do
         message = build(:message, body: nil, image: nil)
         message.valid?
-        expect(message.errors[:content]).to include('を入力してください')
+        expect(message.errors[:body]).to include('を入力してください')
       end
 
       it 'is invalid without group_id' do
