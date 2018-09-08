479c8c2597fb42230fd46192afcf255e78944bc5
沼幹太 <kanta.mischil@icloud.com>
2018-09-08 10:52:13 +0900

    [edit]factorygirl → factorybot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,7 +58,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
