2c81f047bcb286567553420710146b6e405629fa
沼幹太 <kanta.mischil@icloud.com>
2018-09-09 17:07:03 +0900

    [edit]ミスの修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  has_many :groups
 
   validates :body, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
