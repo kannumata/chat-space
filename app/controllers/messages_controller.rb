3cff4ae61bd55450175c90acf1056b5c76e35b41
沼幹太 <kanta.mischil@icloud.com>
2018-09-16 01:28:32 +0900

    細かい修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-
   before_action :set_group
 
   def index
@@ -7,31 +6,19 @@ class MessagesController < ApplicationController
     @messages = @group.messages.includes(:user).order("id ASC")
   end
 
-  # def create
-  #   @message = @group.messages.new(message_params)
-  #   if @message.save
-  #     redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-  #   else
-  #     @messages = @group.messages.includes(:user)
-  #     flash.now[:alert] = 'メッセージを入力してください。'
-  #     render :index
-  #   end
-  # end
-
   def create
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.html {redirect_to group_messages_path(@group)}
-        format.json
+      format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+      format.json
       end
     else
       @messages = @group.messages.includes(:user)
-      flash.now[:alert] = "メッセージを入力してください"
+      flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
   end
-
 
   private
   def message_params
