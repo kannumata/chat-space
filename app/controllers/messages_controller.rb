9b4a307ad2019cfc6337e17b2e4bc819827d1a58
沼幹太 <kanta.mischil@icloud.com>
2018-09-17 01:06:31 +0900

    投稿機能を非同期通信のみにする

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-      format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+      # format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
       format.json
       end
     else
