1fbbf90a3f2e17c7894b51b823b74708f7fb3ce8
沼幹太 <kanta.mischil@icloud.com>
2018-09-17 20:45:16 +0900

    [edit]アクション設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where("name LIKE(?)",  "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
