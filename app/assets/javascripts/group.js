13de27b462ca593a1beca46f00e4b2779f56b270
沼幹太 <kanta.mischil@icloud.com>
2018-09-17 20:46:47 +0900

    [edit]appendUser定義

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,34 +1,32 @@
 $(function(){
 
+  var search_list = $(".chat-group-form")
 
-  function  appendProduct(user){
+  function  appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.data} data-user-name=${user.name}>追加</a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.data}data-user-name=${user.name}>追加</a>
                 </div>`
-  }
-
-
-  function appendNoProduct(user) {
+    search_list.append(html);
   }
 
   $("#user-search-field").on("keyup",function(){
     var input = $("#user-search-field").val();
-
     $.ajax({
       type: 'GET',
       url: '/user/index',
       data: { keyword: input },
       dataType: 'json'
     })
-
     .done(function(users){
-      if (products.length !== 0) {
-        products.forEach(function(user){
-          appendProduct(user);
-    })
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+    })else{
+          alert("一致する名前がありません")
+        }
     .fail(function(){
       alert('ユーザー検索に失敗しました');
     })
-  })
+  });
 });
