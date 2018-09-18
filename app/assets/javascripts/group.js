8888b26ce4e6ef01a92153c9e60cd48a46aa40c7
沼幹太 <kanta.mischil@icloud.com>
2018-09-18 16:35:24 +0900

    細かい調整

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -8,7 +8,6 @@ $(function(){
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.data}data-user-name=${user.name}>追加</a>
                 </div>`
     search_list.append(html);
-    return html;
   }
 
   function  appendNoUser(user){
@@ -19,24 +18,24 @@ $(function(){
 
   $("#user-search-field").on("keyup",function(){
     var input = $(this).val();
-      $.ajax({
-        type: 'GET',
-        url: '/user/index',
-        data: { keyword: input },
-        dataType: 'json'
-      })
-      .done(function(users){
-        $("#user-search-result").empty();
-        if (users.length !== 0) {
-          users.forEach(function(user){
-            appendUser(user);
-        search_list.append(html);
-        })else{
-            appendNoUser("一致する名前がありません")
-          }
-      .fail(function(){
-        alert('ユーザー検索に失敗しました');
-    }
+    $.ajax({
+      type: 'GET',
+      url: '/user/index',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }else{
+          appendNoUser("一致する名前がありません")
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
     })
   });
 });
