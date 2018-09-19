d6f59535dd0c1f20112a534b754ed610a20f9ba5
沼幹太 <kanta.mischil@icloud.com>
2018-09-19 20:15:34 +0900

    細かい変更

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,7 +4,7 @@ $(function(){
   function  appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.data }data-user-name=${ user.name }>追加</a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id }data-user-name=${ user.name }>追加</a>
                 </div>`
     search_list.append(html);
     return html;
--- app/views/messages/index.json.jbuilder
+++ app/views/messages/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array! @users do |user|
   json.name      user.name
-  json.data      user.id
+  json.id      user.id
 end
