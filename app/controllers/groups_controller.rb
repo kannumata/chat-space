53e66374b16bbae8f6cfb4210c55f6a0433c4491
沼幹太 <kanta.mischil@icloud.com>
2018-09-19 23:59:16 +0900

    [edit]記法変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -32,7 +32,7 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, user_ids:[])
   end
 
   def set_group
