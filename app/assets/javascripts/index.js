2b10b5e993faf8373552a8a0bcdb6fcb6e53828d
沼幹太 <kanta.mischil@icloud.com>
2018-09-19 20:43:44 +0900

    [edit]インクリメンタルサーチ完成

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,28 +1,23 @@
 $(function(){
-  var search_list = $("#user_search_result")
+  var search_list = $("#user-search-result");
 
-  function  appendUser(user){
+  function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id }data-user-name=${ user.name }>追加</a>
-                </div>`
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
+               </div>`;
     search_list.append(html);
-    return html;
   }
 
   function  appendNoUser(user){
     var html = `<p class="chat-group-user__name">${ user }</p>`
     search_list.append(html);
-    return html;
   }
 
   $("#user-search-field").on("keyup",function(){
     //検索窓に打ち込まれた時に
     var input = $(this).val();
     //inputに文字列を代入
-    console.log("成功");
-    console.log(input);
-    console.log(this);
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -31,11 +26,10 @@ $(function(){
     })
     .done(function(users){
       search_list.empty();
-      console.log("だん");
-      console.log(users);
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
+          console.log(user)
         });
       }else{
         appendNoUser("一致する名前がありません");
--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,4 +1,6 @@
 json.array! @users do |user|
-  json.name      user.name
+  json.name     user.name
   json.id      user.id
 end
+
+
