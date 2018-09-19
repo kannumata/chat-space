74b4f10f280af9178d70957c960ee046485540bf
沼幹太 <kanta.mischil@icloud.com>
2018-09-19 18:49:07 +0900

    [edit]自分を含まない検索設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where("name LIKE(?)",  "%#{params[:keyword]}%")
+     @users = User.where.not(id: current_user.id).where('name LIKE(?)' , "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
