a83b98674132d44100c076ced55f65d9a545ed0c
沼幹太 <kanta.mischil@icloud.com>
2018-09-19 21:01:07 +0900

    [edit]ユーザー追加機能の実装

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -14,6 +14,14 @@ $(function(){
     search_list.append(html);
   }
 
+  function appendUsertoGroup(user_id, user_name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='z${ user_id }>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+  }
+
   $("#user-search-field").on("keyup",function(){
     //検索窓に打ち込まれた時に
     var input = $(this).val();
@@ -29,7 +37,6 @@ $(function(){
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
-          console.log(user)
         });
       }else{
         appendNoUser("一致する名前がありません");
@@ -39,6 +46,11 @@ $(function(){
       alert('ユーザーの検索に失敗しました');
     })
   });
+  $("#user-search-field").on("click", "user-search-add", function(){
+    // var user_id =
+    // var user_name =
+    appendUsertoGroup(user_id, user_name);
+  } )
 });
 
 
